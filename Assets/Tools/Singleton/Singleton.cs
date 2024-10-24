b2c1d1c055b5723d4d385694c07b683134144f55
theo-bsl <t.basile@lahorde.school>
2024-10-24 10:41:00 +0200

    TerrainGeneration

---
+++ Assets/Tools/Singleton/Singleton.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[DefaultExecutionOrder(-100)]
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+    
+    [Header("Singleton Settings")]
+    [SerializeField] protected bool _needDontDestroyOnLoad = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance) 
+                return _instance;
+            
+            Debug.LogError("New " + typeof(T) + " Instantiated");
+            return new GameObject("New " + typeof(T)).AddComponent<T>();
+
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.LogError(typeof(T) + " Already exist");
+            Destroy(this.gameObject);
+            return;
+        }
+
+        if (_needDontDestroyOnLoad)
+            DontDestroyOnLoad(gameObject);
+
+        _instance = this as T;
+    }
+}
