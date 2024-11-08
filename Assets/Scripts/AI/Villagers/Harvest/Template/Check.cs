87ca3fd663487d812be4f0cc151b1c700e817597
theo-bsl <t.basile@lahorde.school>
2024-11-08 14:32:19 +0100

    Test Template

--- Assets/Scripts/AI/Villagers/Harvest/Template/Check.cs
+++ Assets/Scripts/AI/Villagers/Harvest/Template/Check.cs
@@ -3,23 +3,23 @@ using AI.BT;
 
 public class Check<T> : Node where T : Enum
 {
-    private readonly T _typeToCheck;
+    private readonly T _dataToCheck;
     private readonly string _dataKey;
-    private readonly Func<T, bool> _checkNeed;
+    private readonly Func<T, bool> _checkData;
 
-    public Check(T typeToCheck, string dataKey, Func<T, bool> checkNeed)
+    public Check(T dataToCheck, string dataKey, Func<T, bool> checkData)
     {
-        _typeToCheck = typeToCheck;
+        _dataToCheck = dataToCheck;
         _dataKey = dataKey;
-        _checkNeed = checkNeed;
+        _checkData = checkData;
     }
 
     public override NodeState Evaluate()
     {
-        if (!_checkNeed(_typeToCheck))
+        if (!_checkData(_dataToCheck))
             return NodeState.FAILURE;
         
-        SetDataInParent(_dataKey, _typeToCheck);
+        SetDataInParent(_dataKey, _dataToCheck);
 
         return NodeState.SUCCESS;
     }
